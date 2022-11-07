37ccab57e6a70f7d61a007f94f258835f9cb4c27
Shubham Singh <46527607+ShubhamSingh1112@users.noreply.github.com>
2022-11-08 00:05:53 +0530

    Update ens.php

--- ens.php
+++ ens.php
@@ -1,35 +1,43 @@
 <div class="container-fluid bg-3 text-center" style="border: 1px groove #D3D3D3; border-radius: 5px 5px 5px 5px">
-  <button style=" background-color: black; border-radius: 5px 5px 5px 5px"><h3><a href="homepage.php?xx=s2" style="text-decoration: none; color: white">EXCLUSIVE</a><a href="homepage.php?xx=s2" style="text-decoration: none; color: #86cce3"> NINTENDO</a><a href="XB1.php" style="text-decoration: none; color: #ff3333"> SWITCH</a><a href="XB1.php" style="text-decoration: none; color: white"> GAMES</a></h3></button>
-  <br>
-  <br> 
-  <div class="container-fluid">
-  <div class="row">
-  <div class="col-sm-4">
-  <div class="card" style="border: groove 1px red; box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)">
-    <div class="card-block">
-      <img src="sogimg\mc8d.jpg" style="width: 100%">
-        <p align="center" class="card-title"><a href="">Mario Kart 8 Deluxe</a></p>
-    <h6 align="center" class="card-text">₹3,999</h6>
-    <button class="btn btn-info">Details</button>
+        <button style=" background-color: black; border-radius: 5px 5px 5px 5px">
+            <h3><a href="homepage.php?xx=s2" style="text-decoration: none; color: white">EXCLUSIVE</a>
+                <a href="homepage.php?xx=s2" style="text-decoration: none; color: #86cce3"> NINTENDO</a>
+                <a href="XB1.php" style="text-decoration: none; color: #ff3333"> SWITCH</a>
+                <a href="XB1.php" style="text-decoration: none; color: white"> GAMES</a>
+            </h3>
+        </button>
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-sm-4">
+                    <div class="card" style="border: groove 1px red; box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)">
+                        <div class="card-block">
+                            <img src="sogimg\mc8d.jpg" style="width: 100%">
+                            <p align="center" class="card-title"><a href="">Mario Kart 8 Deluxe</a></p>
+                            <h6 align="center" class="card-text">₹3,999</h6>
+                            <button class="btn btn-info">Details</button>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-4">
+                    <div class="card" style="border: groove 1px red; box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)">
+                        <div class="cars-block">
+                            <img src="sogimg\KSA.jpg" style="width: 100%">
+                            <p align="center" class="card-title"><a href="3.php">Kirby Star Allies</a></p>
+                            <h6 align="center" class="card-text">₹3,999</h6>
+                            <button class="btn btn-info">Details</button>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-4">
+                    <div class="card" style="border: groove 1px red; box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)">
+                        <div class="cars-block">
+                            <img src="sogimg\SMO.jpg" style="width: 100%">
+                            <p align="center" class="card-title"><a href="3.php">Super Mario Odyssey</a></p>
+                            <h6 align="center" class="card-text">₹3,999</h6>
+                            <button class="btn btn-info">Details</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
-  </div>
-  <div class="col-sm-4">
-<div class="card" style="border: groove 1px red; box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)">
-       <div class="cars-block">
-        <img src="sogimg\KSA.jpg" style="width: 100%">
-        <p align="center" class="card-title"><a href="3.php">Kirby Star Allies</a></p>
-        <h6 align="center" class="card-text">₹3,999</h6>
-        <button class="btn btn-info">Details</button>
-    </div>
-  </div>
-  <div class="col-sm-4">
-<div class="card" style="border: groove 1px red; box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)">
-       <div class="cars-block">
-        <img src="sogimg\SMO.jpg" style="width: 100%">
-        <p align="center" class="card-title"><a href="3.php">Super Mario Odyssey</a></p>
-        <h6 align="center" class="card-text">₹3,999</h6>
-        <button class="btn btn-info">Details</button>
-    </div>
-  </div>
-</div>
-</div>
