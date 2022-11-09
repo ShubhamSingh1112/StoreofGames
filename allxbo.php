104a35ba61bbe43edd98ac790817f603ef2e9159
Shubham Singh <46527607+ShubhamSingh1112@users.noreply.github.com>
2022-11-10 00:01:47 +0530

    Update allxbo.php

--- allxbo.php
+++ allxbo.php
@@ -1,71 +1,91 @@
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css">
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" type="text/javascript"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.1/jquery.min.js" type="text/javascript"></script>
-  <body>
-  <div class="container-fluid bg-3 text-center" style="border: 1px groove #D3D3D3; border-radius: 5px 5px 5px 5px">
-  <button style=" background-color: #00b300; border-radius: 5px 5px 5px 5px"><h3><a href="homepage.php" style="text-decoration: none; color: white">All X Box One Games</a></h3></button>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js"
+        type="text/javascript"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.1/jquery.min.js" type="text/javascript"></script>
 
-  <div class="container-fluid">
-  <div class="row">
-  <div class="col-sm-4">
-  <div class="card" style="border: groove 1px red; box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)">
-      <div class="card-block">
-        <img src="sogimg\xdbfz.jpg" style="width: 100%">
-        <p align="center" class="card-title"><a href="">Dragon Ball Fighter Z</a></p>
-		  <h6 align="center" class="card-text">₹4,299</h6>
-      <button class="btn btn-info"><a href="3.php?product_id=12" style="color: white">Details</a></button>
+<body>
+    <div class="container-fluid bg-3 text-center" style="border: 1px groove #D3D3D3; border-radius: 5px 5px 5px 5px">
+        <button style=" background-color: #00b300; border-radius: 5px 5px 5px 5px">
+            <h3><a href="homepage.php" style="text-decoration: none; color: white">All X Box One Games</a></h3>
+        </button>
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-sm-4">
+                    <div class="card"
+                        style="border: groove 1px red; box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)">
+                        <div class="card-block">
+                            <img src="sogimg\xdbfz.jpg" style="width: 100%">
+                            <p align="center" class="card-title"><a href="">Dragon Ball Fighter Z</a></p>
+                            <h6 align="center" class="card-text">₹4,299</h6>
+                            <button class="btn btn-info"><a href="3.php?product_id=12"
+                                    style="color: white">Details</a></button>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-4">
+                    <div class="card"
+                        style="border: groove 1px red; box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)">
+                        <div class="card-block">
+                            <img src="sogimg\xmhw.jpg" style="width: 100%">
+                            <p align="center" class="card-title"><a href="">Monster Hunter World</a></p>
+                            <h6 align="center" class="card-text">₹3,999</h6>
+                            <button class="btn btn-info"><a href="3.php?product_id=13"
+                                    style="color: white">Details</a></button>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-4">
+                    <div class="card"
+                        style="border: groove 1px red; box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)">
+                        <div class="cars-block">
+                            <img src="sogimg\setx.jpg" style="width: 100%">
+                            <p align="center" class="card-title"><a href="PS4/3.php">Sea of Thieves</a></p>
+                            <h6 align="center" class="card-text">₹3,999</h6>
+                            <button class="btn btn-info"><a href="3.php?product_id=14"
+                                    style="color: white">Details</a></button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-sm-4">
+                    <div class="card"
+                        style="border: groove 1px red; box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)">
+                        <div class="card-block">
+                            <img src="sogimg\pubg.jpg" style="width: 100%">
+                            <p align="center" class="card-title"><a href="">Playerunknown's
+                                    Battlegrounds</a></p>
+                            <h6 align="center" class="card-text">₹4,299</h6>
+                            <button class="btn btn-info"><a href="3.php?product_id=2"
+                                    style="color: white">Details</a></button>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-4">
+                    <div class="card"
+                        style="border: groove 1px red; box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)">
+                        <div class="card-block">
+                            <img src="sogimg\t7x.jpg" style="width: 100%">
+                            <p align="center" class="card-title"><a href="">Tekken 7</a></p>
+                            <h6 align="center" class="card-text">₹3,999</h6>
+                            <button class="btn btn-info"><a href="3.php?product_id=2"
+                                    style="color: white">Details</a></button>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-4">
+                    <div class="card"
+                        style="border: groove 1px red; box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)">
+                        <div class="cars-block">
+                            <img src="sogimg\xf18.jpg" style="width: 100%">
+                            <p align="center" class="card-title"><a href="PS4/3.php">FIFA 18</a></p>
+                            <h6 align="center" class="card-text">₹3,999</h6>
+                            <button class="btn btn-info"><a href="3.php?product_id=2"
+                                    style="color: white">Details</a></button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
-  </div>
-
-  <div class="col-sm-4">
-  <div class="card" style="border: groove 1px red; box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)">
-    <div class="card-block">
-      <img src="sogimg\xmhw.jpg" style="width: 100%">
-        <p align="center" class="card-title"><a href="">Monster Hunter World</a></p>
-		<h6 align="center" class="card-text">₹3,999</h6>
-    <button class="btn btn-info"><a href="3.php?product_id=13" style="color: white">Details</a></button>
-    </div>
-  </div>
-  <div class="col-sm-4">
-<div class="card" style="border: groove 1px red; box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)">
-       <div class="cars-block">
-        <img src="sogimg\setx.jpg" style="width: 100%">
-        <p align="center" class="card-title"><a href="PS4/3.php">Sea of Thieves</a></p>
-        <h6 align="center" class="card-text">₹3,999</h6>
-        <button class="btn btn-info"><a href="3.php?product_id=14" style="color: white">Details</a></button>
-    </div>
-  </div>
-</div>
-  <br>
-  <div class="row">
-    <div class="col-sm-4">
-  <div class="card" style="border: groove 1px red; box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)">
-      <div class="card-block">
-        <img src="sogimg\pubg.jpg" style="width: 100%">
-        <p align="center" class="card-title"><a href="">Playerunknown's Battlegrounds</a></p>
-      <h6 align="center" class="card-text">₹4,299</h6>
-      <button class="btn btn-info"><a href="3.php?product_id=2" style="color: white">Details</a></button>
-    </div>
-  </div>
-  <div class="col-sm-4">
-  <div class="card" style="border: groove 1px red; box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)">
-    <div class="card-block">
-      <img src="sogimg\t7x.jpg" style="width: 100%">
-        <p align="center" class="card-title"><a href="">Tekken 7</a></p>
-    <h6 align="center" class="card-text">₹3,999</h6>
-    <button class="btn btn-info"><a href="3.php?product_id=2" style="color: white">Details</a></button>
-    </div>
-  </div>
-  <div class="col-sm-4">
-<div class="card" style="border: groove 1px red; box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)">
-       <div class="cars-block">
-        <img src="sogimg\xf18.jpg" style="width: 100%">
-        <p align="center" class="card-title"><a href="PS4/3.php">FIFA 18</a></p>
-        <h6 align="center" class="card-text">₹3,999</h6>
-        <button class="btn btn-info"><a href="3.php?product_id=2" style="color: white">Details</a></button>
-    </div>
-  </div>
-</div>
-</div>
-</div>
 </body>
