99e7d91694462d15cced20ec8fff06869af381f3
Shubham Singh <46527607+ShubhamSingh1112@users.noreply.github.com>
2022-11-09 23:51:51 +0530

    Update Orderconfirmed.php

--- Orderconfirmed.php
+++ Orderconfirmed.php
@@ -1,6 +1,6 @@
-<link rel="stylesheet" href="Bootstrap/css/bootstrap.min.css">
-  <script src="Bootstrap/js/bootstrap.min.js" type="text/javascript"></script>
-  <script src="Bootstrap/js/jquery.js" type="text/javascript"></script>
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" type="text/javascript"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.1/jquery.min.js" type="text/javascript"></script>
 
 <style>
 	body  {
