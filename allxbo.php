a030b92b509ed4883ef50ddd3a56b6c9e2079590
Shubham Singh <46527607+ShubhamSingh1112@users.noreply.github.com>
2022-11-09 23:56:36 +0530

    Update allxbo.php

--- allxbo.php
+++ allxbo.php
@@ -14,10 +14,9 @@
         <p align="center" class="card-title"><a href="">Dragon Ball Fighter Z</a></p>
 		  <h6 align="center" class="card-text">₹4,299</h6>
       <button class="btn btn-info"><a href="3.php?product_id=12" style="color: white">Details</a></button>
-      </div>
     </div>
   </div>
-  <div class="clearfix visible-xs"></div>
+
   <div class="col-sm-4">
   <div class="card" style="border: groove 1px red; box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)">
     <div class="card-block">
@@ -25,10 +24,8 @@
         <p align="center" class="card-title"><a href="">Monster Hunter World</a></p>
 		<h6 align="center" class="card-text">₹3,999</h6>
     <button class="btn btn-info"><a href="3.php?product_id=13" style="color: white">Details</a></button>
-      </div>
     </div>
   </div>
-  <div class="clearfix visible-xs"></div>
   <div class="col-sm-4">
 <div class="card" style="border: groove 1px red; box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)">
        <div class="cars-block">
@@ -36,7 +33,6 @@
         <p align="center" class="card-title"><a href="PS4/3.php">Sea of Thieves</a></p>
         <h6 align="center" class="card-text">₹3,999</h6>
         <button class="btn btn-info"><a href="3.php?product_id=14" style="color: white">Details</a></button>
-      </div>
     </div>
   </div>
 </div>
@@ -49,10 +45,8 @@
         <p align="center" class="card-title"><a href="">Playerunknown's Battlegrounds</a></p>
       <h6 align="center" class="card-text">₹4,299</h6>
       <button class="btn btn-info"><a href="3.php?product_id=2" style="color: white">Details</a></button>
-      </div>
     </div>
   </div>
-  <div class="clearfix visible-xs"></div>
   <div class="col-sm-4">
   <div class="card" style="border: groove 1px red; box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)">
     <div class="card-block">
@@ -60,10 +54,8 @@
         <p align="center" class="card-title"><a href="">Tekken 7</a></p>
     <h6 align="center" class="card-text">₹3,999</h6>
     <button class="btn btn-info"><a href="3.php?product_id=2" style="color: white">Details</a></button>
-      </div>
     </div>
   </div>
-  <div class="clearfix visible-xs"></div>
   <div class="col-sm-4">
 <div class="card" style="border: groove 1px red; box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)">
        <div class="cars-block">
@@ -71,7 +63,6 @@
         <p align="center" class="card-title"><a href="PS4/3.php">FIFA 18</a></p>
         <h6 align="center" class="card-text">₹3,999</h6>
         <button class="btn btn-info"><a href="3.php?product_id=2" style="color: white">Details</a></button>
-      </div>
     </div>
   </div>
 </div>
