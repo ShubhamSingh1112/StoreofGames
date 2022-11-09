467ac8f6d60a70d826003f60e4301d3eec3ab359
Shubham Singh <46527607+ShubhamSingh1112@users.noreply.github.com>
2022-11-09 23:52:42 +0530

    Update allns.php

--- allns.php
+++ allns.php
@@ -1,6 +1,6 @@
-<link rel="stylesheet" type="text/css" href="Bootstrap\css\bootstrap.min.css">
- <script src="Bootstrap\js\bootstrap.min.js" type="text/javascript"></script>
- <script src="Bootstrap\js\jquery.js" type="text/javascript"></script>
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" type="text/javascript"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.1/jquery.min.js" type="text/javascript"></script>
   <body>
   <div class="container-fluid bg-3 text-center" style="border: 1px groove #D3D3D3; border-radius: 5px 5px 5px 5px">
   <button style=" background-color: #3232ff; border-radius: 5px 5px 5px 5px"><h3><a href="homepage.php" style="text-decoration: none; color: white">All Nintendo Switch Games</a></h3></button>
