c9f8a21ba5a72376c8b96d944176b2aa05043e56
Shubham Singh <46527607+ShubhamSingh1112@users.noreply.github.com>
2022-11-12 23:13:44 +0530

    Update xbocards.php

--- xbocards.php
+++ xbocards.php
@@ -3,12 +3,12 @@
             <h3><a href="homepage.php" style="text-decoration: none; color: white">X BOX LIVE & GAME PASS</a></h3>
         </button>
         <div class="container-fluid">
-            <div class="row">
+            <div class="row m-0">
                 <div class="col-sm-4 mb-4">
                     <div class="card"
                         style="border: groove 1px red; box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)">
                         <div class="card-block">
-                            <img src="sogimg\3m.jpg" style="width: 100%">
+                            <img src="sogimg/3m.jpg" style="width: 100%">
                             <p align="center" class="card-title"><a href="">X Box Live - 3 months</a></p>
                             <h6 align="center" class="card-text">₹999</h6>
                             <button class="btn btn-info">Details</button>
@@ -19,7 +19,7 @@
                     <div class="card"
                         style="border: groove 1px red; box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)">
                         <div class="card-block">
-                            <img src="sogimg\12m.jpg" style="width: 100%">
+                            <img src="sogimg/12m.jpg" style="width: 100%">
                             <p align="center" class="card-title"><a href="">X Box Live - 12 months</a></p>
                             <h6 align="center" class="card-text">₹3,999</h6>
                             <button class="btn btn-info">Details</button>
@@ -29,8 +29,8 @@
                 <div class="col-sm-4 mb-4">
                     <div class="card"
                         style="border: groove 1px red; box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)">
-                        <div class="cars-block">
-                            <img src="sogimg\gp.jpg" style="width: 100%">
+                        <div class="card-block">
+                            <img src="sogimg/gp.jpg" style="width: 100%">
                             <p align="center" class="card-title"><a href="PS4/3.php">X Box Game Pass - 6 months</a></p>
                             <h6 align="center" class="card-text">₹1,699</h6>
                             <button class="btn btn-info">Details</button>
