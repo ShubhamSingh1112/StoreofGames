563ff3a6cf82d865e1a94209fe7e38096aab112e
Shubham Singh <46527607+ShubhamSingh1112@users.noreply.github.com>
2022-11-16 22:57:42 +0530

    Update pscards.php

--- pscards.php
+++ pscards.php
@@ -1,46 +1,43 @@
-<div class="container-fluid bg-3 text-center" style="border: 1px groove #D3D3D3; border-radius: 5px 5px 5px 5px; width: 100%">
-  <button style=" background-color: #3232ff; border-radius: 5px 5px 5px 5px"><h3><a href="homepage.php" style="text-decoration: none; color: white">PLAYSTATION NOW & PLAYSTATION PLUS</a></h3></button>
-  <br>
-  <br> 
-  <div class="container-fluid">
-  <div class="row">
-  <div class="col-sm-4">
-  <div class="card" style="border: groove 1px red; box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)">
-      <div class="card-block">
-        <img src="sogimg\3p.jpg" style="width: 100%">
-        <p align="center" class="card-title"><a href="">Playstation Plus - 3 months</a></p>
-      <h6 align="center" class="card-text">₹999</h6>
-      <button class="btn btn-info">Details</button>
-        <p></p>
+<div class="container-fluid bg-3 text-center"
+   style="border: 1px groove #D3D3D3; border-radius: 5px 5px 5px 5px; width: 100%">
+<button class="mb-4" style=" background-color: #3232ff; border-radius: 5px 5px 5px 5px">
+   <h3><a href="homepage.php" style="text-decoration: none; color: white">PLAYSTATION NOW & PLAYSTATION PLUS</a></h3>
+</button>
+<div class="container-fluid">
+   <div class="row">
+      <div class="col-sm-4">
+         <div class="card"
+            style="border: groove 1px red; box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)">
+            <div class="card-block">
+               <img src="sogimg/3p.jpg" style="width: 100%">
+               <p class="card-title"><a href="">Playstation Plus - 3 months</a></p>
+               <h6 class="card-text">₹999</h6>
+               <button class="btn btn-info">Details</button>
+            </div>
+         </div>
       </div>
-    </div>
-  </div>
-
-  <div class="col-sm-4">
-  <div class="card" style="border: groove 1px red; box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)">
-    <div class="card-block">
-      <img src="sogimg\12p.jpg" style="width: 100%">
-        <p align="center" class="card-title"><a href="">Playstation Plus - 12 months</a></p>
-    <h6 align="center" class="card-text">₹3,999</h6>
-    <button class="btn btn-info">Details</button>
-      <p></p>
+      <div class="col-sm-4">
+         <div class="card"
+            style="border: groove 1px red; box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)">
+            <div class="card-block">
+               <img src="sogimg/12p.jpg" style="width: 100%">
+               <p class="card-title"><a href="">Playstation Plus - 12 months</a></p>
+               <h6 class="card-text">₹3,999</h6>
+               <button class="btn btn-info">Details</button>
+            </div>
+         </div>
       </div>
-    </div>
-  </div>
-  <div class="clearfix visible-xs"></div>
-  <div class="col-sm-4">
-<div class="card" style="border: groove 1px red; box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)">
-       <div class="cars-block">
-        <img src="sogimg\now.jpg" style="width: 100%">
-        <p align="center" class="card-title"><a href="3.php">Playstation Now - Gift Card</a></p>
-        <h6 align="center" class="card-text">₹1,299</h6>
-        <button class="btn btn-info">Details</button>
-        <p></p>
+      <div class="clearfix visible-xs"></div>
+      <div class="col-sm-4">
+         <div class="card"
+            style="border: groove 1px red; box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)">
+            <div class="cars-block">
+               <img src="sogimg/now.jpg" style="width: 100%">
+               <p class="card-title"><a href="3.php">Playstation Now - Gift Card</a></p>
+               <h6 class="card-text">₹1,299</h6>
+               <button class="btn btn-info">Details</button>
+            </div>
+         </div>
       </div>
-    </div>
-  </div>
-</div>
-<br>
-</div>
-</body>
+   </div>
 </div>
